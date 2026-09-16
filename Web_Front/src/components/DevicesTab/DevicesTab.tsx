fb19488993c58d08b2f322a78b06ccfa0eb9e0da
Anthony Kuzub <4483@apkaudio.com>
2026-09-15 20:49:06 -0400

    sync: update content

--- Web_Front/src/components/DevicesTab/DevicesTab.tsx
+++ Web_Front/src/components/DevicesTab/DevicesTab.tsx
@@ -297,7 +297,7 @@ export default function DevicesTab() {
           {/* The whole checkout path, because this one is meant to be pasted:
               APK:OS has not been at the repository root since the DOCKERS reorg,
               and `python3 "APK:OS/server.py"` fails on paste from it. PLAN-333.01. */}
-          {' '}<code>python3 "APK:DOCKERS/APK:audio:WebPortal/SRC/APK:OS/server.py"</code>
+          {' '}<code>python3 "APK:PODS/APK:audio:WebPortal/SRC/APK:OS/server.py"</code>
         </p>
       )}
 
